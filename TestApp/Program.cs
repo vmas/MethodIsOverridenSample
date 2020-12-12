a4f8a0693b6db74c8c83842c45d6b5190ba430af
vmas <vladsv88@gmail.com>
2020-12-12 13:21:45 +0300

    Detect if a method was overridden not using Reflection (C#, CIL).

---
+++ TestApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TestLib;
+
+namespace TestApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var a = new Class1();
+			a.TestImpl();
+
+			var b = new Class2();
+			b.TestImpl();
+
+			var c = new Class3();
+			c.TestImpl();
+		}
+	}
+
+	class Class2 : Class1
+	{
+
+	}
+
+	class Class3 : Class1
+	{
+		public override void Test()
+		{
+			base.Test();
+		}
+	}
+
+}
---
+++ TestLib/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace TestLib
+{
+	public class Class1
+	{
+		public virtual void Test()
+		{
+
+		}
+
+		public void TestImpl()
+		{
+			Console.Write(this.GetType().Name + ".Test() is ");
+			if (Class1Ext.HasOriginalTest(this))
+				Console.Write("not ");
+			Console.WriteLine("overriden");
+		}
+	}
+}
